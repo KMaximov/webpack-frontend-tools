0821b5a65c84a283ec34e8b9bbf27d847a04bf14
Mykola <k.maximov19@gmail.com>
2022-05-23 17:45:43 +0300

    docs: complete lesson 1 task2

---
+++ lesson1/task2/src/scripts/index.js
@@ -0,0 +1,23 @@
+import { initTodoListHandlers } from './todoList.js';
+import { renderTasks } from './renderer.js';
+import { getTasksList } from './tasksGetway.js';
+import { setItem } from './storage.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  getTasksList()
+    .then(tasksList => {
+      setItem('tasksList', tasksList)
+      renderTasks();
+    });
+  ;
+  initTodoListHandlers();
+});
+
+const onStorageChange = event => {
+  if (event.key === 'tasksList') {
+    renderTasks();
+  }
+
+}
+
+window.addEventListener('storage', onStorageChange);
---
+++ lesson1/task2/src/scripts/todoList.js
@@ -0,0 +1,11 @@
+import { createTask } from './createTask.js';
+import { updateTask, deleteTasksItem } from './updateTask.js';
+
+export const initTodoListHandlers = () => {
+  const createBtnElem = document.querySelector('.create-task-btn');
+  createBtnElem.addEventListener('click', createTask);
+
+  const tasksListElem = document.querySelector('.list');
+  tasksListElem.addEventListener('click', updateTask);
+  tasksListElem.addEventListener('click', deleteTasksItem);
+};
