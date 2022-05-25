9dee56f7918941596aae03421415197ac587d03e
Mykola <k.maximov19@gmail.com>
2022-05-25 12:29:52 +0300

    docs: finish TodoList with webpack DevServer

--- lesson8/task2/webpack.config.js
+++ lesson8/task2/webpack.config.js
@@ -4,7 +4,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const webpack = require('webpack');
 
 module.exports = (env, argv) => {
-    const isProduction = argv === 'production';
+    const isProduction = argv.mode === 'production';
     const config = {
     entry: './src/index.js',
     output: {
